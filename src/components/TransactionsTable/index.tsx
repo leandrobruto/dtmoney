a66393d445f6b037458c0e746b6d79ed9190a15d
Leandro <leandrobruto42@gmail.com>
2023-05-17 02:55:42 -0300

    :rocket: Criando contexto

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,11 @@
+import { useContext } from "react";
+import { TransactionsContext } from "../../TransactionsContext";
+
 import { Container } from "./styles";
 
 export function TransactionsTable() {
+    const transactions = useContext(TransactionsContext);
+
     return (
         <Container>
             <table>
@@ -14,18 +19,23 @@ export function TransactionsTable() {
                 </thead>
 
                 <tbody>
-                    <tr>
-                        <td>Desenvolvimento de website</td>
-                        <td className="deposit">R$ 12.000,00</td>
-                        <td>Desenvolbimento</td>
-                        <td>20/02/2023</td>
-                    </tr>
-                    <tr>
-                        <td>Aluguel</td>
-                        <td className="withdraw">- R$ 1.100,00</td>
-                        <td>Casa</td>
-                        <td>17/02/2023</td>
-                    </tr>
+                    {transactions.map(transaction => (
+                        <tr key={transaction.id}>
+                            <td>{transaction.title}</td>
+                            <td className={transaction.type}>
+                                {new Intl.NumberFormat('pt-BR', {
+                                    style: 'currency',
+                                    currency: 'BRL'
+                                }).format(transaction.amount)}
+                            </td>
+                            <td>{transaction.category}</td>
+                            <td>
+                                {new Intl.DateTimeFormat('pt-BR').format(
+                                    new Date(transaction.createdAt)
+                                )}    
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </Container>
